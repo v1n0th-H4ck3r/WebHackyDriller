687bdf8539c2774d9ec6c125530b6c620d6b5044
v1n0th-H4ck3r <vinoth.dayalanofficial17@gmail.com>
2024-02-20 01:03:44 +0530

    updated main and add dns record finder to it

---
+++ main.py
@@ -0,0 +1,70 @@
+import dns.resolver
+import socket
+from colorama import Fore
+from utlis import _dnsenum
+def internet_check():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        print(Fore.GREEN + "\n[!] Internet Connection is Available!")
+        return None
+    except OSError:
+        print(Fore.RED + "\n[=!] Warning! Internet Connection is Unavailable!")
+        return None
+
+def InfoGather():
+    domainName = input("[+] Enter a specific URL to find pages linking to it (e.g., example.com): ")
+    dnsQuery = _dnsenum.dns_enum(domainName)
+    print(dnsQuery)
+
+
+def Scanner():
+    print("Writing the scanner script...")
+
+
+def vulnAss():
+    print("Writing the vulnass script...")
+
+
+def Exploit():
+    print("Writing the exploit script...")
+
+
+def main():
+    print(Fore.CYAN + """
+[+]    
+|
+|    ___ ___       __    ___ ___            __          ______        __ __ __            
+|   |   Y   .-----|  |--|   Y   .---.-.----|  |--.--.--|   _  \ .----|__|  |  .-----.----.
+|   |.  |   |  -__|  _  |.  1   |  _  |  __|    <|  |  |.  |   \|   _|  |  |  |  -__|   _|
+|   |. / \  |_____|_____|.  _   |___._|____|__|__|___  |.  |    |__| |__|__|__|_____|__|  
+|   |:      |           |:  |   |                |_____|:  1    /                         
+|   |::.|:. |           |::.|:. |                      |::.. . /                          
+|   `--- ---'           `--- ---'                      `------'                           
+|   - By Vinoth ()                                                                                    
+|  
+|
+[+]
+    """)
+    internet_check()
+    searchquery=""
+    while 1:
+        print(Fore.CYAN)
+        print("[1] Information Gathering")
+        print("[2] Scanning")
+        print("[3] Vulnerability Assessment")
+        print("[4] Exploitation")
+        print("\n")
+        query_type = input("[+] Select your option:\t")
+        if query_type=="1":
+            InfoGather()
+        elif query_type == "2":
+            Scanner()
+        elif query_type == "3":
+            vulnAss()
+        elif query_type == "4":
+            Exploit()
+        else:
+            print("\n")
+            print(Fore.MAGENTA + "[❌] Warning - You have entered a unknown number to hack!")
+if __name__ == '__main__':
+    main() 
--- utlis/_dnsenum.py
+++ utlis/_dnsenum.py
@@ -1,37 +1,33 @@
 import dns.resolver
-
+from colorama import Fore
 def dns_enum(domain):
     try:
-        # Perform DNS A record lookup
+        print(Fore.RED + "-----------------------------DNS records---------------------------")
         a_records = dns.resolver.query(domain, 'A')
-        print("A Records:")
         for record in a_records:
-            print(record)
+            print(Fore.CYAN + f"[A] -> {str(record)}")
 
         # Perform DNS AAAA record lookup
         a_records = dns.resolver.query(domain, 'AAAA')
-        print("AAAA Records:")
         for record in a_records:
-            print(record)
+            print(Fore.CYAN + f"[AAAA] -> {str(record)}")
 
         # Perform DNS MX record lookup
         mx_records = dns.resolver.query(domain, 'MX')
-        print("\nMX Records:")
         for record in mx_records:
-            print(record)
+            print(Fore.CYAN + f"[MX] -> {str(record)}")
 
         # Perform DNS NS record lookup
         ns_records = dns.resolver.query(domain, 'NS')
-        print("\nNS Records:")
         for record in ns_records:
-            print(record)
+            print(Fore.CYAN + f"[NS] -> {str(record)}")
 
         # Perform DNS TXT record lookup
         txt_records = dns.resolver.query(domain, 'TXT')
-        print("\nTXT Records:")
         for record in txt_records:
-            print(record)
+            print(Fore.CYAN + f"[TXT] -> {str(record)}")
 
+        print(Fore.RED + "--------------------------------------------------------------------")
     except dns.resolver.NoAnswer:
         print("No DNS records found.")
     except dns.resolver.NXDOMAIN:
@@ -42,7 +38,3 @@ def dns_enum(domain):
         print("No nameservers found for the domain.")
     except Exception as e:
         print("An error occurred:", e)
-
-if __name__ == "__main__":
-    domain = input("Enter the domain to perform DNS enumeration: ")
-    dns_enum(domain)
